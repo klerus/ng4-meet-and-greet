77f92531e69c18d14797309e759626f5990d5c86
Rafał Jaworski <rafal.jaworski@bravelab.io>
2017-07-15 14:04:13 +0200

    Add filtering

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,22 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule} from '@angular/platform-browser'
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { ProductsListComponent } from './products-list/products-list.component';
 import { ProductTileComponent } from './product-tile/product-tile.component';
+import { SearchComponent } from './search/search.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ProductsListComponent,
-    ProductTileComponent
+    ProductTileComponent,
+    SearchComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/products-list/products-list.component.ts
+++ src/app/products-list/products-list.component.ts
@@ -1,24 +1,38 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import * as _ from 'lodash';
 
 @Component({
   selector: 'app-products-list',
   templateUrl: './products-list.component.html',
-  styleUrls: [
-    './products-list.component.css']
+  styleUrls: ['./products-list.component.css']
 })
 export class ProductsListComponent implements OnInit {
 
-  public products: Array<Product> = [];
+  public products: Product[] = [];
+  public filteredProducts: Product[] = [];
+  public searchInput = new FormControl();
+
+  public sort (button) {
+    this.filteredProducts.sort(function (a,b) {
+      return a.price - b.price;
+    };
+  }
+
   constructor() {
+    this.searchInput.valueChanges.subscribe(value => this.filteredProducts = this.products.filter(
+      product => product.has(value.toLowerCase()))
+  );
+
     this.products.push(new Product('Coffee', 5.99, false));
     this.products.push(new Product('Tea', 2.99, false));
     this.products.push(new Product('Burger', 15.99, true));
     this.products.push(new Product('Pizza', 30.99, true));
     this.products.push(new Product('Tiger', 2.99, true));
+    this.filteredProducts = this.products;
   }
 
-  ngOnInit() {
-  }
+  ngOnInit() {}
 }
 
 class Product {
@@ -33,6 +47,9 @@ class Product {
     this.promoted = promoted;
     this.imgURL = 'https://dummyimage.com/318x200/000/fff';
   }
+  public has(value: string) {
+    return _.some(_.map([this.name, this.price], function (n) {
+      return n.toString().toLowerCase().indexOf(value) >= 0;
+    }));
+  }
 }
-
-
